406931f1359b4589b3965150423bcbd74c06401e
jerrygacket <jerrygacket@gmail.com>
2019-06-14 11:13:57 +0300

    Эксперименты с CORS

--- back/controllers/MainController.php
+++ back/controllers/MainController.php
@@ -42,16 +42,20 @@ class MainController extends Controller
 
     public function behaviors()
     {
-        return ArrayHelper::merge([
+        return ArrayHelper::merge(parent::behaviors(), [
             'corsFilter'  => [
                 'class' => Cors::class,
                 'cors' => [
                     'Origin' => ['*'],
                     'Access-Control-Request-Method' => ['POST','GET', 'HEAD', 'OPTIONS'],
-                    'Access-Control-Allow-Credentials' => null,
+                    'Access-Control-Allow-Credentials' => false,
                     'Access-Control-Max-Age' => 3600,
+                    'Access-Control-Request-Headers' => ['*'],
+                    'Access-Control-Expose-Headers' => [],
+                    'Access-Control-Allow-Origin' => ['*'],
+
                 ],
             ],
-        ], parent::behaviors());
+        ]);
     }
 }
