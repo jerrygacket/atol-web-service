eb8b5dbbc74ca97c4fe9d8aff93d10828c5a907d
jerrygacket <jerrygacket@gmail.com>
2019-06-21 15:58:57 +0300

    Добавлен функционал для работы с железным принтером.

--- back/components/PrinterComponent.php
+++ back/components/PrinterComponent.php
@@ -6,26 +6,44 @@ namespace app\components;
 
 use app\base\BasicComponent;
 use app\models\Printers;
+use Yii;
 
 
 class PrinterComponent extends BasicComponent
 {
-    public function getAll() {
-        $model = $this->getModel();
+    public $model;
 
+    /**
+     * @var $model Printers
+     * @param string $id
+     * @return Printers
+     */
+    public function getModel($params) {
+        $model = new $this->nameClass(
+            $this->nameClass::findOne(
+                ['printer_id' => ($params['info']['printer_id'] ?? '')]
+            )
+        );
+
+        return $model;
+    }
+
+    /**
+     * @param $model Printers
+     * @return mixed
+     */
+    public function getAll($model) {
         return $model::find()->cache(10)->all();
     }
 
-    /*
-     * @param array $params
-     * @var $model Printers
+    /**
+     * @param $model Printers
+     * @return mixed
      */
-    public function getOne($params) {
-        $model = $this->getModel();
-        $model->load($params);
-
+    public function getOne($model) {
         return $model::find()
-            ->where(['printer_id' => $params['printer_id']])
+            ->where(['printer_id' => $model->printer_id])
+            ->cache(10)
             ->one();
     }
 
@@ -38,31 +56,67 @@ class PrinterComponent extends BasicComponent
     }
 
     /**
-     * @param $params
-     * @return mixed
-     * @var $model Printers
+     * @param $model Printers
+     * @return bool
+     * @throws \yii\base\ExitException
      */
-    public function openShift($params) {
-        $model = $this->getModel();
-        $model->load($params['info']['printer_id']);
-        $result = $model->openShift($params['info']['operator']);
+    public function openShift($model) {
+        $result = false;
+        if (!$model->isShiftOpen()) {
+            $response = $model->openShift();
+            if ($response) {
+//                $result['shift_open'] = ($response['results'][0]['result']['deviceStatus']['shift'] == 'open');
+//                $result['connected'] = ($response['results'][0]['status'] == 'ready');
+                print_r($response);
+                Yii::$app->end();
+                $result = true;
+            }
+        }
 
         return $result;
     }
 
-    public function closeShift($params) {
-        $model = $this->getModel();
-        $model->load($params['info']['printer_id']);
-        $result = $model->closeShift();
+    /**
+     * @param $model Printers
+     * @return mixed
+     */
+    public function closeShift($model) {
+        $result = false;
+        if ($model->isShiftOpen()) {
+            $response = $model->closeShift();
+            if ($response) {
+//                $result['shift_open'] = ($response['results'][0]['result']['deviceStatus']['shift'] == 'open');
+//                $result['connected'] = ($response['results'][0]['status'] == 'ready');
+                print_r($response);
+                Yii::$app->end();
+                $result = true;
+            }
+        }
 
         return $result;
     }
 
     public function printSellReceipt($params) {
         $model = $this->getModel();
-        $model->load($params['info']['printer_id']);
-        $result = $model->printSellReceipt($params['']);
+        $model->load($params['info']['printer_id'] ?? '');
+        $result = $model->printSellReceipt($params);
 
-        return false;
+        return $result;
+    }
+
+    /**
+     * @param $model Printers
+     * @return mixed
+     */
+    public function status($model) {
+        $response = $model->checkKKT();
+        if ($response) {
+            $result['shift_open'] = ($response['results'][0]['result']['deviceStatus']['shift'] == 'open');
+            $result['connected'] = ($response['results'][0]['status'] == 'ready');
+        } else {
+            $result['error'] = 'Error while get status';
+        }
+
+        return $result;
     }
 }
